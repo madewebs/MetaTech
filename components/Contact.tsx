7c20b4eed67f9fd6abbc8ac35ee88c6b814384a5
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-11-08 01:19:54 +0530

    added

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -222,7 +222,7 @@ export default function Contact() {
                 <h3 className='text-xl font-semibold p-8 pb-4'>Our Location</h3>
                 <div className='w-full h-[250px]'>
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.2412648750455!2d-73.98823492346679!3d40.74844097138558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1699999999999!5m2!1sen!2sus"
+                     src="https://www.google.com/maps?q=MQ3F%2B9X8,+Pimpri-Chinchwad,+Maharashtra&z=17&output=embed"
                     width="100%"
                     height="100%"
                     style={{ border: 0 }}
